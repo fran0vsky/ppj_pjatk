effe24b8b7ad5bd49807a238c57b539fc335ae81
Franek Kaminski <franekaminski@gmail.com>
2023-12-06 23:29:09 +0100

    directories structure

---
+++ src/courses/tim_buchalka/first_steps/variables.java
@@ -0,0 +1,12 @@
+package courses.tim_buchalka.first_steps;
+
+public class variables {
+    public static void main(String[] args) {
+        int myFirstNumber = 10;
+        System.out.println(myFirstNumber);
+        System.out.println(myFirstNumber + 10);
+        System.out.println(myFirstNumber / 5);
+        System.out.println(myFirstNumber * 3);
+        System.out.println(((myFirstNumber + 5) * 3) / 5);
+    }
+}
