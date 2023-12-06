4bd931a3bccfa02f5be21de1864661d67116656c
Franek Kaminski <franekaminski@gmail.com>
2023-12-06 23:05:17 +0100

    directories structure

---
+++ src/courses/tim_buchalka/first_steps/hello_world.java
@@ -0,0 +1,8 @@
+package courses.tim_buchalka.first_steps;
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello, Franek");
+    }
+}
