c434ab37917ec1d3c3926fadea6bd6a2ca647fab
Franek Kaminski <franekaminski@gmail.com>
2023-12-08 22:46:31 +0100

    directories structure

---
+++ src/courses/tim_buchalka/intellij_Basics/hello_world_in_intellij.java
@@ -0,0 +1,7 @@
+package courses.tim_buchalka.intellij_Basics;
+
+public class hello_world_in_intellij {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
